b710e31995369515a6efe4fd4f00dce9cace469e
LiYX <493234798@qq.com>
2014-09-15 22:58:08 +0800

    byliyuxing9/15

--- TakeOut/app/controllers/Manager.java
+++ TakeOut/app/controllers/Manager.java
@@ -11,16 +11,26 @@ public class Manager extends Controller{
 	
 	public static void addMenu(Menu menu){
 		menu.create();
+		flash.success("success to add the menu!");
+		index();
 	}
 	
-	public static void modifyPrice(Menu menu,double price){
-		
+	public static void modifyMenu(Long id){
+		Menu menu = Menu.findById(id);
+	    menu.edit("menu", params.all());
+	    validation.valid(menu);
+	    if(validation.hasErrors()) {
+	        menu.refresh();
+	        flash.success("fail to modify the menu!");
+	    }
+	    flash.success("success to modify the menu!");
+	    index();
 	}
 	
 	public static void deleteMenu(Menu menu){
 		menu.delete();
+		flash.success("success to delete the %s", menu.name);
+		index();
 	}
-	
-	
 	
 }
